a158c07184a19b01d157b5657beb7ae9e4af365c
raffaeler <raffaeler@vevy.com>
2021-04-15 22:23:09 +0200

    Initial commit

---
+++ CMakeProjectRepro/CMakeProjectRepro/CMakeProjectRepro.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProjectRepro.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProjectRepro.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
